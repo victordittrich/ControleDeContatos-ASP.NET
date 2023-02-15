7d1f3e0c6de72b283b0265712f8f6d0f86749e82
Victor Hugo <vituhds0610@gmail.com>
2023-02-15 19:52:41 -0300

    att

---
+++ ControleDeContatos/ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public int Email { get; set; }
+        public int Telefone { get; set; }
+
+
+
+    }
+}
